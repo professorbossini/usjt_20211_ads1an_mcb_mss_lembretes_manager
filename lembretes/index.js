3ea9b0bb43809f36df816253e00fbf23e6e1b904
Sergio Bonato <asbonato@gmail.com>
2021-03-16 23:28:28 -0300

    Lembretes sem eventos pronto

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,13 +1,23 @@
 const express = require ('express');
 const app = express();
+app.use(express.json());
+const lembretes = {};
+let contador = 0;
 
 //http://localhost:porta/lembretes
 app.get('/lembretes', (req, res) => {
-
+    res.send(lembretes);
 })
 
 app.put ('/lembretes', (req, res) => {
-
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador]);
 })
 
-app.listen(4000, () => console.log("Lembretes:4000"));
+app.listen(4000, () => {
+    console.log("Lembretes: porta 4000");
+});
