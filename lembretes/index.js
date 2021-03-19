7efee64cd7abb7515b9129eb6ae53ba8ba5cae8d
Sergio Bonato <asbonato@gmail.com>
2021-03-18 21:51:00 -0300

    Lembrete atualizado para chamar barramento de eventos e com endpoind eventos

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 const lembretes = {};
@@ -9,13 +10,25 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 })
 
-app.put ('/lembretes', (req, res) => {
+app.put ('/lembretes', async (req, res) => {
     contador++;
     const { texto } = req.body;
     lembretes[contador] = {
         contador, texto
     }
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "Lembrete Criado",
+        dados: { 
+            contador,
+            texto,
+        },
+    })
     res.status(201).send(lembretes[contador]);
+})
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok"});
 })
 
 app.listen(4000, () => console.log("Lembretes:4000"));
