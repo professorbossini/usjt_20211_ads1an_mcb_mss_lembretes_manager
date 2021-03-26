e5a851247c6c341e3602fd6a81d715848c452824
Sergio Bonato <asbonato@gmail.com>
2021-03-25 23:29:26 -0300

    comentada a chamada ao endpoint eventos no serviço obervações

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -7,8 +7,9 @@ app.post('/eventos', (req, res) => {
     const evento = req.body;
     //envia o evento para o microsserviço de lembretes
     axios.post('http://localhost:4000/eventos', evento);
-     //envia o evento para o microsserviço de observacoes
-    axios.post('http://localhost:5000/eventos', evento);
+     //envia o evento para o microsserviço de observacoes 
+     //comentado pois falta implementar o endpoint eventos no serviço de observações
+    //axios.post('http://localhost:5000/eventos', evento);
     res.status(200).send({ msg: "ok"});
 });
 
